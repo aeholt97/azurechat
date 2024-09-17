bb93365f0ea89676b4a2bf72b1118bfe62aa1f41
aeholt97 <74876293+aeholt97@users.noreply.github.com>
2024-09-17 17:39:47 +0200

    Update chat-home.tsx remove Extensions from home page

--- src/features/chat-home-page/chat-home.tsx
+++ src/features/chat-home-page/chat-home.tsx
@@ -35,26 +35,6 @@ export const ChatHome: FC<ChatPersonaProps> = (props) => {
         ></Hero>
         <div className="container max-w-4xl flex gap-20 flex-col">
           <div>
-            <h2 className="text-2xl font-bold mb-3">Extensions</h2>
-
-            {props.extensions && props.extensions.length > 0 ? (
-              <div className="grid grid-cols-3 gap-3">
-                {props.extensions.map((extension) => {
-                  return (
-                    <ExtensionCard
-                      extension={extension}
-                      key={extension.id}
-                      showContextMenu={false}
-                    />
-                  );
-                })}
-              </div>
-            ) :
-              <p className="text-muted-foreground max-w-xl">No extentions created</p>
-            }
-
-          </div>
-          <div>
             <h2 className="text-2xl font-bold mb-3">Personas</h2>
 
             {props.personas && props.personas.length > 0 ? (
