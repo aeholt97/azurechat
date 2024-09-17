f725215c8907cd9826692a74cb4315e7b34843c5
aeholt97 <74876293+aeholt97@users.noreply.github.com>
2024-09-17 13:35:38 +0200

    Update unauthorized page with better text layout.tsx

--- src/app/(authenticated)/unauthorized/layout.tsx
+++ src/app/(authenticated)/unauthorized/layout.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const HelloWorld: React.FC = () => (
   <div>
-    Hello world
+    You have not been registered as a user of Medjour Azurechat. If you believe you should have access contact your system administrator.
+    (if you already had access, sorry, but we are trying to add specific user groups. More info will on e-mail follow soon)
   </div>
 );
 
