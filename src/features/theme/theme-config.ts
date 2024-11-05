fc15d831abf86e1fdaf2756e3bef59b1f2197843
aeholt97 <74876293+aeholt97@users.noreply.github.com>
2024-11-05 15:37:34 +0100

    Updated text in theme-config.ts to remove note about privacy

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,9 +1,8 @@
 export const AI_NAME = "Medjour Chat";
 export const AI_DESCRIPTION = `Medjour Chat is a friendly AI assistant. It runs GPT-4o in the background and is available to all staff and some Master's students at the department of Media and Journalism Studies at Aarhus University. 
-If you need to chat with files or generate images use Copilot (https://copilot.microsoft.com/). For everything else, this should be better. 
+If you need to chat with files or generate images use Copilot (copilot.microsoft.com). For everything else, this should be better. 
 You can create Personas to chat with for specialized tasks or use some of the available preprompts from the prompt library (these will be updated at some point). 
-To get started, press the "New Chat" button on the upper left or select one of the Personas below.
-NOTE: All data will be accessible to administrators.`;
+To get started, press the "New Chat" button on the upper left or select one of the Personas below.`;
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
